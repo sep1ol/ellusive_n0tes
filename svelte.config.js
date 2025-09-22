30148a86298669a1aed4fe4a91c32bdbb0b106b7
sep1ol <sepiol.dev@gmail.com>
2025-09-22 16:57:05 -0300

    add svelte adapter to svelte-config file

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,9 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter()
+	}
 };
 
 export default config;
